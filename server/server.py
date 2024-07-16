739f1b933850f79699182d5a343aa85780543296
Franci <francesca1999@yahoo.com>
2024-07-16 15:36:00 +0300

    add get artist by id endpoint

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 import database
 from flask import Flask, request, jsonify
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 db = database.setup()
 db_data = db["data"]
+
 
 @app.route('/artists', methods=["GET"])
 def get_artists():
@@ -16,5 +18,15 @@ def get_artists():
 
     return jsonify({'data': json_list}), 200
 
+@app.route('/artists/<artist_id>')
+def get_artist(artist_id):
+    document = db_data.find_one({'_id':ObjectId(artist_id)})
+    document['_id'] = str(document['_id'])
+
+    if document is None:
+        return jsonify({'error':'no document was found!'}), 404
+
+    return jsonify({'data':document}), 200
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
